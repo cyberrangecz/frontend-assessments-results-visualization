97610f7869b0d0e0648ec22190d52fd546df6193
Hai Duong Tran <445437@mail.muni.cz>
2018-09-14 18:20:50 +0200

    Show answers for every choice

--- projects/kypo2-assessments-results-visualization-lib/src/lib/components/multiple-choice-question-results/multiple-choice-question-results.component.ts
+++ projects/kypo2-assessments-results-visualization-lib/src/lib/components/multiple-choice-question-results/multiple-choice-question-results.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MCQ } from '../../models/mcq';
 import { MCQAnswer } from '../../models/mcqanswer';
+import { MCQChoice } from '../../models/mcqchoice';
 
 @Component({
   selector: 'kypo2-viz-assessments-results-multiple-choice-question-results',
@@ -14,6 +15,9 @@ export class MultipleChoiceQuestionResultsComponent implements OnInit {
   questionTitle: string;
   answers: MCQAnswer[];
   order: number;
+  choices: MCQChoice[];
+
+  answersCount: any;
 
   constructor() { }
 
@@ -21,6 +25,27 @@ export class MultipleChoiceQuestionResultsComponent implements OnInit {
     this.questionTitle = this.MCQData.text;
     this.answers = this.MCQData.answers;
     this.order = this.MCQData.order;
+    this.choices = this.MCQData.choices;
+
+    this.countAnswers();
+  }
+
+  countAnswers() {
+    this.initializeAnswersCount();
+    
+    this.answers.forEach((answer: MCQAnswer) => {
+      answer.choices.forEach((choice: number) => {
+        this.answersCount[choice].push(answer.userName);
+      });
+    });
+  }
+
+  initializeAnswersCount() {
+    this.answersCount = {};
+    const correctChoices: MCQChoice[] = this.choices;
+    correctChoices.forEach((answer: MCQChoice) => {
+      this.answersCount[answer.order] = [];
+    });
   }
 
 }
